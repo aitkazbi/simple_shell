5dd13d1ff745526ad628e027d0d99259e1f78346
root <root@DESKTOP-2JD5QV2>
2023-05-23 20:11:33 +0100

    Update 4

--- run_cmd.c
+++ run_cmd.c
@@ -3,34 +3,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "ssh.h"
-/*#include "ssh.h"
 
-void run_cmd(char *cmd)
-{
-	char *env[] = {NULL};
-	pid_t pid;
-	pid = fork();
-	if (pid < 0)
-	{				
-		printf("Error");
-		exit(-1);
-	}
-	else if (pid == 0)
-	{
-	 execve(cmd, &cmd, env);
-	}
-	else
-	{
-		wait (NULL);
-	}
-}*/
 
 void run_cmd(char *cmd)
 { 
 	char *args[2];
 
     pid_t pid = fork();
-    
+
     if (pid < 0)
     {
         printf("Error");
@@ -38,7 +18,7 @@ void run_cmd(char *cmd)
     }
     else if (pid == 0)
     {
-        /* enfant */
+        
 
         args[0] = cmd;
         args[1] = NULL;
@@ -51,7 +31,7 @@ void run_cmd(char *cmd)
     }
     else
     {
-        /* parent*/
+     
         wait(NULL);
     }
 }
