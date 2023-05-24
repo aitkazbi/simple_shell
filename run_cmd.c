d37a528727e93e3d954985be7650a62df1244486
aitkazbi <khadija.aitkazbi@gmail.com>
2023-05-24 18:19:50 +0100

    seconde update May 24

--- run_cmd.c
+++ run_cmd.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include "ssh.h"
 
-void run_cmd(char *cmd)
+/*void run_cmd(char *cmd)
 {
     pid_t pid = fork();
 
@@ -16,6 +16,36 @@ void run_cmd(char *cmd)
     else if (pid == 0)
     {
         if (system(cmd) == -1)
+        {
+            printf("Failed command\n");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        wait(NULL);
+    }
+}
+*/
+void run_cmd(char *cmd)
+{
+    pid_t pid = fork();
+ char *envp[] = {NULL};
+    if (pid < 0)
+    {
+        printf("Error\n");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        char *args[4];
+        args[0] = "/bin/sh";
+        args[1] = "-c";
+        args[2] = cmd;
+        args[3] = NULL;
+        
+        if (execve("/bin/sh", args, envp) == -1)
         {
             printf("Failed command\n");
             exit(EXIT_FAILURE);
