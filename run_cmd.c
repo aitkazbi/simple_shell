e8f7cf1549a53f694c48d9ca1619cc7a4293ee1c
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-25 01:04:27 +0100

    AutoPush

--- run_cmd.c
+++ run_cmd.c
@@ -14,10 +14,7 @@ void run_cmd(char *cmd, char **argv)
 	pid = fork();
 
 	if (pid < 0)
-	{
-		_puts("Error\n", STDERR_FILENO);
-		exit(-1);
-	}
+		exit(127);
 	else if (pid > 0)
 	{
 		waitpid(pid, &status, 0);
@@ -26,8 +23,7 @@ void run_cmd(char *cmd, char **argv)
 	{
 		if (execve(cmd, argv, environ) == -1)
 		{
-			_puts("Failed command\n", STDERR_FILENO);
-			exit(1);
+			exit(127);
 		}
 		exit(0);
 	}
