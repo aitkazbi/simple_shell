0f6f1d121e1a4943f75d26882aa73ff219ae6cd5
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-26 23:04:32 +0100

    AutoPush

--- main.c
+++ main.c
@@ -116,7 +116,8 @@ int main(int ac, char *argv[])
 		if (retGetLine == -1)
 			free_all(&data, true, EXIT_SUCCESS);
 		get_arg(&data);
-		get_path_cmd(&data);
+		if (data.cmd)
+			get_path_cmd(&data);
 		if (data.path_cmd)
 			run_cmd(data.path_cmd, data.arg);
 		else if (isWhiteSpace(data.line))
