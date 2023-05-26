cb05c98680b3d1545be9f263e47283d675d28216
SamirOuarrak <samir.ouarrak@gmail.com>
2023-05-26 23:22:32 +0100

    AutoPush

--- test.bash
+++ test.bash
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Description for the intranet check (one line, support Markdown syntax)
-# Spaces only (medium)
+# Execute `/bin/ls`
 
 ################################################################################
 # The variable 'compare_with_sh' IS OPTIONNAL
@@ -21,7 +21,7 @@
 # as follows: "echo $shell_input | ./hsh"
 #
 # It can be empty and multiline
-shell_input="                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               "
+shell_input="/bin/ls"
 
 ################################################################################
 # The variable 'shell_params' IS OPTIONNAL
